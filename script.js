7e5c538f850d8ec02516cd38ae3a39105b590502
valeria wabwire <larriebliss@gmail.com>
2024-04-10 12:03:27 +0300

    mode button

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   function fetchmeals() {
-//          fetch("http://localhost:4000/meals1").then(res=>res.json()).then( data =>{
-//             let meals = data.data.meals
+document.addEventListener("DOMContentLoaded", function() {
+  function fetchmeals() {
+         fetch("http://localhost:4000/meals1").then(res=>res.json()).then( data =>{
+            let meals = data.data.meals
 
-//     for (let meals of meals){
-//         let img = document.createElement('img')
-//         let p = document.createElement('p')
-//         p.textContent = meals.name
-//         img.src= meals.url
-//         memeSection.append(p,img)
-//     };
-// });
-//   }
-//             .then(resp => resp.json())
-//             .then(data => {
+    for (let meals of meals){
+        let img = document.createElement('img')
+        let p = document.createElement('p')
+        p.textContent = meals.name
+         img.src= meals.url
+        memeSection.append(p,img)
+         };
+});
+  }
+             .then(resp => resp.json())
+             .then(data => {
 //                 meals = data || [];
 //                 rendermeals();
 //             })
@@ -22,23 +22,25 @@
 //             });
 //     }
 // });
-// toggleMode.addEventListener('click', ()=>{
-//     if(allContainers[0].style.backgroundColor==='black'){
-//         allContainers.forEach(container=> {
-//             container.style.backgroundColor='white'
-//         })
-//         allTxts.forEach(txt=> {
-//             txt.style.color ='black'
-//         })
+const toggleMode = document.querySelector('button#mode');
+ toggleMode.addEventListener('click', ()=>{
+    if(allContainers[0].style.backgroundColor==='black'){
+       allContainers.forEach(container=> {
+            container.style.backgroundColor='white'
+        })
+        allTxts.forEach(txt=> {
+            txt.style.color ='black'
+        });
         
-//     }else{
+   }else{
 
-//         allContainers.forEach(container=> {
-//             container.style.backgroundColor='black'
-//         })
-//         allTxts.forEach(txt=> {
-//             txt.style.color ='white'
-//         })
+       allContainers.forEach(container=> {
+             container.style.backgroundColor='black'
+        })
+        allTxts.forEach(txt=> {
+            txt.style.color ='white'
+       })
         
-//     }
-// })
+    }
+ })
+});
